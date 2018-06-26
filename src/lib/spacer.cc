ddb9b5085254e30677d17cf569cfcab66ba12006
George Madrid <gmadrid@gmail.com>
2018-06-26 18:18:37 -0400

    Updating comment.

--- src/lib/spacer.cc
+++ src/lib/spacer.cc
@@ -1,9 +1,4 @@
 #include "spacer.h"
 
-// LeitnerBox:
-//   Buckets: vector<vector<Question>
-// There will be 8 of these: 1-7, plus 0 (which has questions still not
-// introduced).
-//
 // Schedule:
 //   Sessions: vector<vector<bucket numbers>>
